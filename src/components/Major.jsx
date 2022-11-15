e012d8261d32f224615c2a7aec6c96ea855853aa
sirgama <sirengogamaliel@gmail.com>
2022-11-15 19:16:18 +0300

    add styling to home hero section

--- src/components/Major.jsx
+++ src/components/Major.jsx
@@ -5,7 +5,7 @@ import requests from '../Requests'
 export default function Major() {
     const [movies, setMovies] = useState([])
 
-    const  singleMovie = movies[Math.floor(Math.random() * movies.length)]
+    
     const getMovies = () =>{
         axios.get(requests.requestPopular).then((res) => {
 
@@ -14,8 +14,32 @@ export default function Major() {
         })
     }
     useEffect(()=> getMovies(), [])
-    console.log(singleMovie.overview)
+    let singleMovie = movies[Math.floor(Math.random() * movies.length)]
+    const truncateString = (str, num) =>{
+        if(str?.length > num){
+            return str.slice(0, num) + '...'
+        }
+        else{
+            return str
+        }
+    }
+
+    console.log(movies)
   return (
-    <div>Major</div>
+    <div className="w-full h-[500px] text-white">
+    <div className='w-full h-full'>
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+        <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original${singleMovie?.backdrop_path}`} alt={singleMovie?.title} />
+        <div className='absolute top-[20%] p-5 m-5'>
+            <h1 className='text-3xl md:text-5xl font-extrabold '>{singleMovie?.title}</h1>
+            <div className='my-4'>
+                <button className='text-black border bg-gray-300 px-5 py-2'>Play</button>
+                <button className='text-white border  px-5 py-2 ml-4 font-medium'>Watch later</button>
+            </div>
+            <p className='text-gray-400 text-sm'>Release date: {singleMovie?.release_date}</p>
+            <p className="w-full text-gray-100 text-sm py-2 md:max-w-[70%] lg:max-w-[50%] xl:max-w-30%">{truncateString(singleMovie?.overview, 150)}</p>
+        </div>
+    </div>
+    </div>
   )
 }
