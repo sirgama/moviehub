85f8897dc313bc3fef00cc2006bba4b720c63cb3
sirgama <sirengogamaliel@gmail.com>
2022-11-15 22:26:03 +0300

    add scroll effect on movies section

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -5,6 +5,7 @@ import requests from '../Requests'
 
 export default function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([])
+    const [likes, setLikes] = useState(false)
 
     const getMovies = () => {
         axios.get(fetchUrl).then((res) => {
@@ -17,12 +18,15 @@ export default function Row({title, fetchUrl}) {
     <div className='md:mt-8'>
       <h2 className="text-white font-bold md:text-xl p-4 ">{title}</h2>
       <div className="relative flex items-center">
-        <div id={'slider'}>
+        <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
             {movies.map((item, id) => (
                 <div key={item.id} className='w-[160px] sm:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-3'>
                     <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`} alt={item.title} />
                     <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-80 text-white">
                         <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center '>{item?.title}</p>
+                        <p>
+                            {likes? <FaHeart className='absolute top-4 left-4 text-gray-400' /> : <FaRegHeart  className='absolute top-4 left-4 text-gray-400'/>}
+                        </p>
                     </div>
                 </div>
             ))}
