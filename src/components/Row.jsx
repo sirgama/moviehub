71c7983481f3a792ed1ef056fd3dd76951e2dbb6
sirgama <sirengogamaliel@gmail.com>
2022-11-15 22:13:15 +0300

    fix trending error 34

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import {FaHeart, FaRegHeart} from 'react-icons/fa'
 import React, { useEffect, useState } from 'react'
 import requests from '../Requests'
 
@@ -18,10 +19,10 @@ export default function Row({title, fetchUrl}) {
       <div className="relative flex items-center">
         <div id={'slider'}>
             {movies.map((item, id) => (
-                <div className='w-[160px] sm:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-3'>
+                <div key={item.id} className='w-[160px] sm:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-3'>
                     <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`} alt={item.title} />
-                    <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-
+                    <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-80 text-white">
+                        <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center '>{item?.title}</p>
                     </div>
                 </div>
             ))}
