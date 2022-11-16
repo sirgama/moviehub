08623518fb183ad178f2642517ce850c620c9ed6
sirgama <sirengogamaliel@gmail.com>
2022-11-17 02:47:46 +0300

    create protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { AuthContextProvider } from "./context/AuthContext";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Account from "./pages/Account";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -17,10 +18,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path='/home' element={<Home />} />
+        <Route path='/home' element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
-        <Route path='/account' element={<Account />} />
+        <Route path='/account' element={<ProtectedRoute><Account /></ProtectedRoute> } />
       </Routes>
       </BrowserRouter>
       </AuthContextProvider>
