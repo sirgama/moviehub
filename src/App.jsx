8323a5a404194549947840368c452302a80d9fef
sirgama <sirengogamaliel@gmail.com>
2022-11-16 23:09:06 +0300

    fix memory router issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Landing from "./pages/Landing";
 import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 
 function App() {
   return (
     <>
     <AuthContextProvider>
-      <Navbar />
+      
       <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path='/home' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/account' element={<Account />} />
       </Routes>
       </BrowserRouter>
       </AuthContextProvider>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Logo from '../assets/logo.svg'
 
 export default function Navbar() {
@@ -6,8 +7,12 @@ export default function Navbar() {
     <div className='flex items-center justify-between p-8 z-[100] absolute w-full'>
         <img src={Logo} width={170} alt="Netflix" />
         <div>
+            <Link to='/login'>
             <button className='text-white bg-red-600 px-5 py-2 rounded mx-2 cursor-pointer'>Sign in</button>
+            </Link>
+            <Link to='/signup'>
             <button className='text-white bg-red-600 px-5 py-2 rounded mx-2 cursor-pointer'>Sign up</button>
+            </Link>
         </div>
     </div>
   )
