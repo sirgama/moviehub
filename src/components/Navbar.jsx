edbd1d04f79cdfce575d4052117685dbb250b29f
sirgama <sirengogamaliel@gmail.com>
2022-11-17 02:28:41 +0300

    create login functinality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ import { UserAuth } from '../context/AuthContext'
 
 
 export default function Navbar() {
-const {user, logout} = UserAuth()
+const {user, logOut} = UserAuth()
 const navigate = useNavigate()
 // console.log(user.metadata.lastSignInTime)
 const handleLogout = async () =>{
     try {
-        await logout()
+        await logOut()
         navigate('/signup')
     } catch (error) {
         console.log(error)
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 export default function Login() {
+    const [email, setEmail] = useState('')
+    const [errorr, setError] = useState('')
+    const [password, setPassword] = useState('')
+    const {user, login} = UserAuth()
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setError('')
+        try {
+            await login(email, password)
+            navigate('/home')
+        } catch (error) {
+            console.log(error)
+            setError(error.message)
+        }
+    }
+
+   
   return (
     <div>
       <div className='w-full h-screen '>
@@ -11,12 +31,19 @@ export default function Login() {
             <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
                 <div className="max-w-[320px] mx-auto py-16">
                     <h1 className="text-3xl font-bold text-center">Get back in</h1>
-                    <form action="" className='w-full flex flex-col py-4'>
-                        <input className='p-3 my-2 bg-gray-700 rounded' type="email" name="email" id="email" autoComplete='email' placeholder='Email' />
-                        <input className='p-3 my-2 bg-gray-700 rounded' type="password" name="" id="" autoComplete='current-password' placeholder='Password' />
+                    {errorr && <div className="flex p-4 mb-4 text-sm text-yellow-700 bg-yellow-100 rounded-lg dark:bg-yellow-200 dark:text-yellow-800" role="alert">
+                    <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                    <span className="sr-only">Info</span>
+                    <div>
+                        <span className="font-medium"></span> {errorr}.
+                    </div>
+                    </div>}
+                    <form onSubmit={handleSubmit} action="" className='w-full flex flex-col py-4'>
+                        <input onChange={(e) => setEmail(e.target.value)} className='p-3 my-2 bg-gray-700 rounded' type="email" name="email" id="email" autoComplete='email' placeholder='Email' />
+                        <input onChange={(e) => setPassword(e.target.value)} className='p-3 my-2 bg-gray-700 rounded' type="password" name="" id="" autoComplete='current-password' placeholder='Password' />
                         <button type="submit" className='bg-red-600 px-6 py-3 rounded text-gray-30 my-6 font-semibold'>Log in</button>
                         <div className='flex justify-between items-center text-sm text-gray-500'>
-                            <p><input type="checkbox" name="" id="" /> Remember me</p>
+                            <p><input type="checkbox" name="checkRef" id="checkbox" /> Remember me</p>
                             <p>Need help?</p>
                         </div>
                         <p className='py-9'><span className='text-gray-500 text-sm mx-1'>Don't have an account?</span>
