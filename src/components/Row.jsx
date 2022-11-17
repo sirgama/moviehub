cc429a39cf6bd3c734c8e972ac406dd27a10825f
sirgama <sirengogamaliel@gmail.com>
2022-11-17 14:13:33 +0300

    deployment configurations

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -3,10 +3,35 @@ import { FaHeart, FaRegHeart } from 'react-icons/fa'
 import React, { useEffect, useState } from 'react'
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 import requests from '../Requests'
+import { UserAuth } from '../context/AuthContext'
+import { db } from '../firebase/firebaseConfigs'
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore'
+
 
 export default function Row({ title, fetchUrl, rowId }) {
     const [movies, setMovies] = useState([])
+    const [saved, setSaved] = useState(false)
     const [likes, setLikes] = useState(false)
+    const {user} = UserAuth()
+    movies.map(item => item)
+    const movieID = doc(db, 'users', `${user?.email}`)
+    const saveShow = async () => {
+        if(user?.email){
+            setLikes(!likes)
+            setSaved(true)
+            await updateDoc(movieID, {
+                savedShows : arrayUnion({
+                    id: item.id,
+                    title: item.title,
+                    img: item.backdrop_path
+                })
+            })
+        } else{
+            console.log('please log in')
+        }
+    }
+
+
     let slider = document.getElementById('slider' + rowId)
     const slideLeft = () => {
         slider.scrollLeft = slider.scrollLeft + 500
@@ -34,7 +59,7 @@ export default function Row({ title, fetchUrl, rowId }) {
                             <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`} alt={item.title} />
                             <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-80 text-white">
                                 <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center '>{item?.title}</p>
-                                <p>
+                                <p onClick={saveShow}>
                                     {likes ? <FaHeart className='absolute top-4 left-4 text-gray-400' /> : <FaRegHeart className='absolute top-4 left-4 text-gray-400' />}
                                 </p>
                             </div>
