2127907a25937bba3303fec5a2943b1bbe522dd4
sirgama <sirengogamaliel@gmail.com>
2022-11-20 22:41:45 +0300

    configure tv endpoints

--- src/Requests.js
+++ src/Requests.js
@@ -8,7 +8,13 @@ const requests = {
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${food}&language=en-US&page=1`,
     requestUpcoming2: `https://api.themoviedb.org/3/movie/upcoming?api_key=${food}&language=en-US&page=2`,
     requestNowplaying: `https://api.themoviedb.org/3/movie/now_playing?api_key=${food}&language=en-US&page=1`,
-    requestNowplaying2: `https://api.themoviedb.org/3/movie/now_playing?api_key=${food}&language=en-US&page=2`
+    requestNowplaying2: `https://api.themoviedb.org/3/movie/now_playing?api_key=${food}&language=en-US&page=2`,
+    tvPopular: `https://api.themoviedb.org/3/tv/popular?api_key=${food}&language=en-US&page=1`,
+    tvLatest: `https://api.themoviedb.org/3/tv/latest?api_key=${food}&language=en-US`,
+    tvToday: `https://api.themoviedb.org/3/tv/airing_today?api_key=${food}&language=en-US&page=1`,
+    tvOnair: `https://api.themoviedb.org/3/tv/on_the_air?api_key=${food}&language=en-US&page=1`,
+    tvToprated: `https://api.themoviedb.org/3/tv/top_rated?api_key=${food}&language=en-US&page=1`,
+    
 }
 
 export default requests
