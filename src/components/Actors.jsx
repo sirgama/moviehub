84dc591b38c50632660d496009d4859aca51fcb8
sirgama <sirengogamaliel@gmail.com>
2022-11-22 17:44:30 +0300

    fix margins and padding on actors images

--- src/components/Actors.jsx
+++ src/components/Actors.jsx
@@ -23,7 +23,7 @@ console.log(actors)
         {actors.map((actor) => (
             <div className='w-[146px] h-[167px] sm:w-[166px] sm:h-[197px] p-4'>
                 <img className='w-full h-full rounded-2xl object-cover m-4 ' src={actor?.profile_path ? `https://image.tmdb.org/t/p/w500${actor?.profile_path}` : `${ProfileImage}`} alt="" />
-                <h3 className=' text-xs font-medium text-white text-center'>{actor.name}</h3>
+                <h3 className=' text-xs sm:text-sm font-medium text-white text-center'>{actor.name}</h3>
                 <h3 className=''>{actor?.character} </h3>
             </div>
         ))}
