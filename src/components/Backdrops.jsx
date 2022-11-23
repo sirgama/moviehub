ac2f439d1f2203858d6f0ded7ad27afb8f4d3418
sirgama <sirengogamaliel@gmail.com>
2022-11-23 15:53:11 +0300

    populate backdrops in tvdetail

--- src/components/Backdrops.jsx
+++ src/components/Backdrops.jsx
@@ -18,6 +18,14 @@ export default function Backdrops({fetchUrl, pathto}) {
 
 
   return (
-    <div>Backdrops</div>
+    <div className='flex flex-row flex-wrap sm:w-5/6 justify-center mx-auto mt-10'>
+        {backdrops.map((item) => (
+            <Link to={`${pathto}${item.id}`} relative="path"><div key={item.id} className='w-[336px] h-[227px] md:w-[386px] md:h-[257px] p-6  '>
+                <img className='w-full h-full rounded-2xl object-cover m-2 relative transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' src={item?.backdrop_path && `https://image.tmdb.org/t/p/original${item?.backdrop_path}` } alt="" />
+                
+            </div>
+            </Link>
+        ))}
+    </div>
   )
 }
