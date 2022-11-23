063f8cfc5b460c0bbc9945f01b7a9e9133a141ef
sirgama <sirengogamaliel@gmail.com>
2022-11-23 15:56:30 +0300

    fetch posters in their component

--- src/components/Posters.jsx
+++ src/components/Posters.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
-export default function Posters() {
+export default function Posters({fetchUrl, pathto}) {
+    const [posters, setposters] = useState([])
+
+    const getposters = () => {
+        axios.get(fetchUrl).then((req) => {
+            const thebacks = req.data.posters
+            setposters(thebacks)
+        })  
+    }
+
+    useEffect(() => {
+        getposters()
+    }, [fetchUrl])
   return (
-    <div>Posters</div>
+    <div className='flex flex-row flex-wrap sm:w-5/6 justify-center mx-auto mt-10'>
+    {posters.map((item) => (
+        <Link to={`${pathto}${item.id}`} relative="path"><div key={item.id} className='w-[336px] h-[227px] md:w-[386px] md:h-[257px] p-6  '>
+            <img className='w-full h-full rounded-2xl object-cover m-2 relative transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' src={item?.backdrop_path && `https://image.tmdb.org/t/p/original${item?.backdrop_path}` } alt="" />
+            
+        </div>
+        </Link>
+    ))}
+</div>
   )
 }
