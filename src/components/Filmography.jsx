a58cba7b3f37b0e8b25761ba55789b34917fa944
sirgama <sirengogamaliel@gmail.com>
2022-11-24 08:38:17 +0300

    create compnent to fetch filmography for individual actors

---
+++ src/components/Filmography.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Filmography({fetchUrl, movie, tv}) {
+
+    const [filmography, setfilmography] = useState([])
+
+    const getfilmography = () => {
+        axios.get(fetchUrl).then((req) => {
+            const therecos = req.data.cast
+            setfilmography(therecos)
+        })
+    }
+
+    useEffect(() => {
+            getfilmography()
+    }, [fetchUrl])
+
+  return (
+    <div>
+        <div className='flex flex-row flex-wrap sm:w-5/6 justify-center mx-auto mt-10'>
+        {filmography.map((item) => (
+            <Link to={item.media_type === 'movie' ? `${movie}${item.id}` : `${tv}${item.id}`} ><div key={item.id} className='w-[336px] h-[227px] md:w-[386px] md:h-[257px] p-6  '>
+                <img className='w-full h-full rounded-2xl object-cover m-2 relative transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' src={item?.backdrop_path && `https://image.tmdb.org/t/p/original${item?.backdrop_path}` } alt="" />
+                <h3 className=' text-base sm:text-base font-medium text-white text-center'>{item.name ? item.name : item.title}</h3>
+                
+            </div>
+            </Link>
+        ))}
+    </div>
+    </div>
+  )
+}
