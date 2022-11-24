db7177be98a642999c4002f9703c26a2b205fc57
sirgama <sirengogamaliel@gmail.com>
2022-11-24 10:42:02 +0300

    add router link to actors component to redirect to individual actor

--- src/components/Actors.jsx
+++ src/components/Actors.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import ProfileImage from '../assets/profile.png'
 
 export default function Actors({fetchUrl}) {
@@ -20,11 +21,11 @@ export default function Actors({fetchUrl}) {
   return (
     <div className='flex flex-row flex-wrap w-full sm:w-5/6 justify-center mx-auto'>
         {actors.map((actor) => (
-            <div key={actor?.id} className='w-[156px] h-[187px] sm:w-[186px] sm:h-[207px] p-6 mt-10 '>
+            <Link to={`/actor/${actor.id}`} ><div key={actor?.id} className='w-[156px] h-[187px] sm:w-[186px] sm:h-[207px] p-6 mt-10 '>
                 <img className='w-full h-full rounded-2xl object-cover m-2 relative transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300' src={actor?.profile_path ? `https://image.tmdb.org/t/p/w500${actor?.profile_path}` : `${ProfileImage}`} alt="" />
                 <h3 className=' text-xs sm:text-sm font-medium text-white text-center'>{actor.name}</h3>
                 
-            </div>
+            </div></Link>
         ))}
     </div>
   )
